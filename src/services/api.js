ef141e562d78e616c95856d619fc23219fe03b9f
Matt Ryan <44824894+mattryanmtl@users.noreply.github.com>
2021-01-10 12:53:33 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const API = axios.create({
   baseURL: "https://api.weatherbit.io/v2.0/"
 });
-export const key = "YOUR_API_KEY";
+export const key = "dabc510102a44ddca2127a89ab8f3a44";
